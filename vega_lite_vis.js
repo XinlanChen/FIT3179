48d81f6872918af33e8de02c1920be90a5dc4f37
XinlanChen <70829474+XinlanChen@users.noreply.github.com>
2021-10-13 16:28:38 +0800

    Assignment 2 Draft
    used for critical

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,10 +1,20 @@
-var vg_1 = "map_vis1.json";
+var vg_1 = "Code/map_vis1.json";
 vegaEmbed('#map_vis1', vg_1).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
-var vg_2 = "barchart_v2.json";
+var vg_2 = "Code/barchart_v2.json";
 vegaEmbed('#barchart_v2', vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "Code/heatmap_v3.json";
+vegaEmbed('#heatmap_v3', vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "Code/linechart_v4.json";
+vegaEmbed('#linechart_v4', vg_4).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
