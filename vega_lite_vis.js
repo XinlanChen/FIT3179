292152d90f6531e1c5e7a711892aae3590919396
XinlanChen <70829474+XinlanChen@users.noreply.github.com>
2021-10-16 17:14:18 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,20 +1,21 @@
 var vg_1 = "map_vis1.json";
-vegaEmbed('#map_vis1', vg_1).then(function(result) {
+vegaEmbed('#map_vis1', vg_1, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 
 var vg_2 = "barchart_v2.json";
-vegaEmbed('#barchart_v2', vg_2).then(function(result) {
+vegaEmbed('#barchart_v2', vg_2, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_3 = "heatmap_v3.json";
-vegaEmbed('#heatmap_v3', vg_3).then(function(result) {
+vegaEmbed('#heatmap_v3', vg_3, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
 var vg_4 = "linechart_v4.json";
-vegaEmbed('#linechart_v4', vg_4).then(function(result) {
+vegaEmbed('#linechart_v4', vg_4, {"actions": false}).then(function(result) {
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
+
